d4b5ba4bbb006925eeb69387eaf5dc5fbff06d8b
Kenta-Mitani <ubuntu@ip-172-31-10-125.ap-northeast-1.compute.internal>
2022-11-15 08:00:19 +0000

    CRUD messages

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,29 @@
+{{-- resources/views/layouts/app.blade.php の内容を継承して利用することを宣言 --}}
+@extends('layouts.app')
+
+@section('content')
+    <div class="prose ml-4">
+        <h2>タスク 一覧</h2>
+    </div>
+
+    @if (isset($tasks))
+        <table class="table table-zebra w-full my-4">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>メッセージ</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <td><a class="link link-hover text-info" href="{{ route('tasks.show', $task->id) }}">{{ $task->id }}</a></td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+    {{-- メッセージ作成ページへのリンク --}}
+    <a class="btn btn-primary" href="{{ route('tasks.create') }}">新規メッセージの投稿</a>
+@endsection
